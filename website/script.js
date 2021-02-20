aa197d3230aa499474277bad8bea39d93d682a51
Emily Kobayashi <emikoifish@gmail.com>
2021-02-20 11:32:08 -0800

    adding more js

--- website/script.js
+++ website/script.js
@@ -1,20 +1,30 @@
 var wordList = ["Kind", "Amusing", "Loyal", "Thoughtful", "Creative", "Compassionate", "Gentle", "Helpful", "Funny"];
 // Needs to be a list from API**
 
-function randomizeWords(){
-    var currList = [];
 
-    while(currList.length < 10){
+var currList = [];
+function randomizeWords() {
+    currList = [];
+    while (currList.length < 9) {
         var currIndex = Math.floor(Math.random() * wordList.length);
         // returns a random integer from list of wordList
         var currWord = wordList[currIndex];
         //console.log(wordList[currIndex]);
-        if(!(currList.includes(currWord))){
+        if (!(currList.includes(currWord))) {
             currList.push(currWord);
         }
-
-
     }
     console.log(currList);
-    return currList;
+    for(i=0; i<9; i++){
+        assignWordToGrid(i)
+    }
+    
 }
+
+
+function assignWordToGrid(i){
+    document.getElementById(i.toString()).innerHTML = currList[i];
+    return currList[i];
+}
+
+
